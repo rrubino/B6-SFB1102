2e8cf0c3dcd5fe0461c9a0302d4ac87715dc6f4a
Ahmad Taie <s8ahtaie@stud.uni-saarland.de>
2016-09-13 14:19:19 +0200

    Fixed to run and updated syllableRatio.

--- infodens/featurextractor/featuremanager.py
+++ infodens/featurextractor/featuremanager.py
@@ -2,7 +2,7 @@ import importlib
 import imp, os
 import sys, inspect
 from os import path
-from utils import featid, idsOfMethods
+from .utils import featid, idsOfMethods
 
 # Remove when callExtractors is implemented
 from .surfaceFeatures import SurfaceFeatures
@@ -13,9 +13,10 @@ class FeatureManager:
     And call the necessary feature extractors.
     """
 
-    def __init__(self, featureIDs, featureArgs):
+    def __init__(self, featureIDs, featureArgs, listOfSentences):
         self.featureIDs = featureIDs
         self.featureArgs = featureArgs
+        self.lofs = listOfSentences
 
     def checkFeatValidity(self):
         """TODO: Check if features requested are valid. """
@@ -37,77 +38,78 @@ class FeatureManager:
 
         #Testing feature and example.
         # Init the class with the corresponding argument
-        #surfaceFeats = SurfaceFeatures(self.featureArgs[0])
+        surfaceFeats = SurfaceFeatures(self.lofs,self.featureArgs[0])
         # call the needed function
-        #print(surfaceFeats.averageWordLength())
+        print(surfaceFeats.averageWordLength())
+        print(surfaceFeats.syllableRatio())
 
-        #print("Called features")
+        print("Called features")
         '''
         extract all feature Ids and names
         
         '''
-        if __package__ is None:
-            sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-        file, pathname, description = imp.find_module('featurextractor')
-
-        possFeatureClasses = set([os.path.splitext(module)[0] for module in os.listdir(pathname) if module.endswith('.py')])
-        
-        modules  = []
-        featureIds = {}  #All feature Ids
-        
-        for eachName in possFeatureClasses:
-            if eachName.startswith('__'):
-                pass
-            else:
-                modd = __import__('featurextractor.'+eachName)
-                modules.append(getattr(modd, eachName))        
-        
-        for modul in modules:
-            clsmembers = inspect.getmembers(modul, inspect.isclass)
-            for i in range(len(clsmembers)):        
-                featureIds = idsOfMethods(clsmembers[i][1], 'featid', featureIds)
-        
-
-        '''
-        extract feature Ids and names chosen by user
-        
-        '''        
-        featIDByUser = {} #fid selected by user + names of methods
-        for fid in self.featureIDs:
-            if fid in featureIds.keys():
-                featIDByUser[fid] = featureIds[fid]
-        
-        
-        
-        '''
-        extract all feature Ids and classes and methods in a dictionary
-        
-        '''
-        idClassmethod = {}
-           
-        for modul in modules:
-            clsmembers = inspect.getmembers(modul, inspect.isclass)
-            for i in range(len(clsmembers)):        
-                fIDs = idsOfMethods(clsmembers[i][1], 'featid', {})
-                for key in fIDs:
-                    if key in self.featureIDs:
-                        idClassmethod[key] = clsmembers[i][1]
-                        
-        ##dictionary = {id: class}
-                            
-                            
-        '''
-        extract features
-        
-        '''
-        featuresExtracted = []
-        for i in range(len(self.featureIDs)):
-            mtdCls = idClassmethod[self.featureIDs[i]]
-            instance = mtdCls(self.featureArgs[i])
-            methd = getattr(instance, featureIds[self.featureIDs[i]])
-            featuresExtracted.append(methd())
+        # if __package__ is None:
+        #     sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        # file, pathname, description = imp.find_module('featurextractor')
+        #
+        # possFeatureClasses = set([os.path.splitext(module)[0] for module in os.listdir(pathname) if module.endswith('.py')])
+        #
+        # modules  = []
+        # featureIds = {}  #All feature Ids
+        #
+        # for eachName in possFeatureClasses:
+        #     if eachName.startswith('__'):
+        #         pass
+        #     else:
+        #         modd = __import__('featurextractor.'+eachName)
+        #         modules.append(getattr(modd, eachName))
+        #
+        # for modul in modules:
+        #     clsmembers = inspect.getmembers(modul, inspect.isclass)
+        #     for i in range(len(clsmembers)):
+        #         featureIds = idsOfMethods(clsmembers[i][1], 'featid', featureIds)
+        #
+        #
+        # '''
+        # extract feature Ids and names chosen by user
+        #
+        # '''
+        # featIDByUser = {} #fid selected by user + names of methods
+        # for fid in self.featureIDs:
+        #     if fid in featureIds.keys():
+        #         featIDByUser[fid] = featureIds[fid]
+        #
+        #
+        #
+        # '''
+        # extract all feature Ids and classes and methods in a dictionary
+        #
+        # '''
+        # idClassmethod = {}
+        #
+        # for modul in modules:
+        #     clsmembers = inspect.getmembers(modul, inspect.isclass)
+        #     for i in range(len(clsmembers)):
+        #         fIDs = idsOfMethods(clsmembers[i][1], 'featid', {})
+        #         for key in fIDs:
+        #             if key in self.featureIDs:
+        #                 idClassmethod[key] = clsmembers[i][1]
+        #
+        # ##dictionary = {id: class}
+        #
+        #
+        # '''
+        # extract features
+        #
+        # '''
+        # featuresExtracted = []
+        # for i in range(len(self.featureIDs)):
+        #     mtdCls = idClassmethod[self.featureIDs[i]]
+        #     instance = mtdCls(self.featureArgs[i])
+        #     methd = getattr(instance, featureIds[self.featureIDs[i]])
+        #     featuresExtracted.append(methd())
             
-        return featuresExtracted
+        #return featuresExtracted
             
         
                                 
--- infodens/featurextractor/surfaceFeatures.py
+++ infodens/featurextractor/surfaceFeatures.py
@@ -4,34 +4,34 @@ Created on Sun Sep 04 14:12:49 2016
 
 @author: admin
 """
-from utils import featid
+from .utils import featid
 
 
 class SurfaceFeatures:
     
     
-    def __init__(self, lof):
+    def __init__(self, lof,argumentString):
         '''
         Initializes the class with a list of sentences (or blocks)
         
         '''
         self.listOfSentences = lof
+        self.featArg = argumentString
     
     @featid(1)    
     def averageWordLength(self):
-        '''
-        Finds average word length of every sentence (or block)
-        by counting the length of every word divided by the number of words in the sentence
-        
-        '''
+        '''Find average word length of every sentence and return list. '''
+
         aveWordLen = []
         for sentence in self.listOfSentences:
+            sentence = sentence.strip()
+            sentence = sentence.split()
             length = sum([len(s) for s in sentence])
-            aveWordLen.append(float(length)/ len(sentence))
-            
+            aveWordLen.append(float(length) / len(sentence))
+
         return aveWordLen
-        
-    @featid(2)    
+
+    @featid(2)
     def syllableRatio(self):
         '''
         We approximate this feature by counting the number of vowel-sequences
@@ -42,6 +42,8 @@ class SurfaceFeatures:
         vowels = ['a', 'e', 'i', 'o', 'u']
         sylRatios = []
         for sentence in self.listOfSentences:
+            sentence = sentence.strip()
+            sentence = sentence.split()
             sylCount = 0
             for word in sentence:
                 word2List = list(word)
