437779db1dcab01f76b1ec55cf354cc62e1f291f
Ahmad Taie <s8ahtaie@stud.uni-saarland.de>
2016-09-13 15:34:30 +0200

    Updated controller: handle file with, split config read func.

--- infodens/controller/controller.py
+++ infodens/controller/controller.py
@@ -10,44 +10,39 @@ class Controller:
         self.featureIDs = []
         self.featargs = []
         self.listOfSent = []
+        self.inputClasses = []
+        self.classifiersList = []
+        self.inputFile = 0
 
-    def loadConfig(self):
-        """Read the config file, extract the featureIDs and
-        their argument strings.
-        """
-        config = open(self.config, 'r')
+    def parseConfig(self, configFile):
+        """Parse the config file lines.      """
         statusOK = 1
-        inputFile = 0
-        inputClasses = 0
-        classifiersList = 0
 
-
-        # Extract featureID and feature Argument string
-        for line in config:
-            line = line.strip()
-            if len(line) < 1:
-                #Line is empty
+        for configLine in configFile:
+            configLine = configLine.strip()
+            if len(configLine) < 1:
+                # Line is empty
                 continue
-            elif line[0] is '#':
-                #Line is comment
+            elif configLine[0] is '#':
+                # Line is comment
                 continue
-            elif "input" in line:
-                startInp = line.index(':')
-                line = line[startInp+1:]
-                line = line.strip()
-                line = line.split()
-                inputFile = line[0]
-                inputClasses = line[1]
-                print(inputFile)
-                print(inputClasses)
-            elif "classif" in line:
-                startInp = line.index(':')
-                line = line[startInp+1:]
-                line = line.strip()
-                classifiersList = line.split()
-                print(classifiersList)
+            elif "input" in configLine:
+                # Extract input files
+                startInp = configLine.index(':')
+                configLine = configLine[startInp + 1:]
+                configLine = configLine.strip().split()
+                self.inputFile = configLine[0]
+                self.inputClasses = configLine[1]
+                print(self.inputFile)
+                print(self.inputClasses)
+            elif "classif" in configLine:
+                startInp = configLine.index(':')
+                configLine = configLine[startInp + 1:]
+                configLine = configLine.strip().split()
+                self.classifiersList = configLine
+                print(self.classifiersList)
             else:
-                params = line.split()
+                params = configLine.split()
                 if len(params) == 2:
                     if params[0].isdigit():
                         self.featureIDs.append(params[0])
@@ -60,12 +55,25 @@ class Controller:
                     statusOK = 0
                     print("Incorrect number of params, should be only 2")
 
-        if inputFile is 0:
-            print("Error, Input file not found. ")
-            statusOK = 0
-        else:
-            preprocessor = preprocess.Preprocess(inputFile)
-            self.listOfSent = preprocessor.preprocessBySentence()
+        return statusOK
+
+    def loadConfig(self):
+        """Read the config file, extract the featureIDs and
+        their argument strings.
+        """
+        statusOK = 0
+
+        # Extract featureID and feature Argument string
+        with open(self.config) as config:
+            # Parse the config file
+            statusOK = self.parseConfig(config)
+
+            if self.inputFile is 0:
+                print("Error, Input file not found. ")
+                statusOK = 0
+            else:
+                preprocessor = preprocess.Preprocess(self.inputFile)
+                self.listOfSent = preprocessor.preprocessBySentence()
 
         config.close()
 
