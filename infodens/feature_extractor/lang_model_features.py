86dce5d8e5346fcfe7593f7bceca289c78853712
Ahmad Taie <s8ahtaie@stud.uni-saarland.de>
2017-06-22 16:20:24 +0200

    fixes to feat 17 and 18

--- infodens/feature_extractor/lang_model_features.py
+++ infodens/feature_extractor/lang_model_features.py
@@ -141,7 +141,7 @@ class Lang_model_features(Feature_extractor):
 
         srilmBinary, kenlm = self.preprocessor.getBinariesPath()
 
-        if srilmBinary:
+        if srilmBinary and not kenlm:
             pplFile = "tempLang{0}{1}.ppl".format(os.path.basename(taggedInput), ngramOrder)
 
             command = "\"{0}ngram\" -order {1} -lm {2} -ppl {3} -debug 1 -unk> {4}".format(srilmBinary, ngramOrder,
@@ -152,14 +152,23 @@ class Lang_model_features(Feature_extractor):
             os.remove(pplFile)
             return sparse.lil_matrix(probab)
         else:
-            import pynlpl.lm.lm as pineApple
-            arpaLM = pineApple.ARPALanguageModel(langModel[1:-1])
-            probab = []
-            # TODO: get Tokenized POS (from taggedInput)
-            for sent in self.preprocessor.gettokenizeSents():
-                probab.append([arpaLM.score(sent)])
-            output = sparse.lil_matrix(probab)
-            return output
+            try:
+                __import__('imp').find_module('kenlm')
+                import kenlm
+                model = kenlm.Model(langModel[1:-1])
+                probab = []
+                for sent in self.preprocessor.getPOStagged():
+                    probab.append([model.score(sent, bos=True, eos=True)])
+                output = sparse.lil_matrix(probab)
+                return output
+            except ImportError:
+                import pynlpl.lm.lm as pineApple
+                arpaLM = pineApple.ARPALanguageModel(langModel[1:-1])
+                probab = []
+                for sent in self.preprocessor.getPOStagged():
+                    probab.append([arpaLM.score(sent)])
+                output = sparse.lil_matrix(probab)
+                return output
 
     def getSplits(self, counts, sumcounts, splitCount):
         splits = []
