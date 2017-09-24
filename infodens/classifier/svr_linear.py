e5421d0733289aa538350d281b37e29a9235c149
ahmad_taie <a.gilani@hotmail.com>
2017-09-24 20:28:02 +0200

    override classif class in svr

--- infodens/classifier/svr_linear.py
+++ infodens/classifier/svr_linear.py
@@ -8,6 +8,7 @@ from infodens.classifier.classifier import Classifier
 from sklearn.svm import LinearSVR
 from sklearn.model_selection import GridSearchCV
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
 import time
 
@@ -19,6 +20,12 @@ class SVR_linear(Classifier):
 
     classifierName = 'Support Vector Regressor'
     C = np.logspace(-5.0, 5.0, num=10, endpoint=True, base=2)
+
+    def evaluate(self):
+
+        """ Overriding default evaluate"""
+        y_pred = self.predict()
+        return mean_squared_error(self.ytest, y_pred)
 
     def train(self):
 
@@ -34,3 +41,22 @@ class SVR_linear(Classifier):
               start_time, ' seconds')
 
         self.model = clf.best_estimator_
+
+    def runClassifier(self):
+        """ Overriding default running"""
+        mse = []
+        #pre = []; rec = []; fsc = []
+
+        for i in range(self.n_foldCV):
+            self.shuffle()
+            self.splitTrainTest()
+            self.train()
+            error = self.evaluate()
+            mse.append(error)
+
+        classifReport = 'Average MSE: ' + str(np.mean(mse))
+        #classifReport += '\nAverage Precision: ' + str(np.mean(pre))
+        #classifReport += '\nAverage Recall: ' + str(np.mean(rec))
+        #classifReport += '\nAverage F-score: ' + str(np.mean(fsc))
+
+        return classifReport
