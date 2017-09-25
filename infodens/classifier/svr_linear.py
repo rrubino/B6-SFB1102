779dc04f8190d74aea66f82bb0c0194d9ab12bd5
rrubino <raphael.rubino@gmail.com>
2017-09-25 15:48:18 +0200

    Add metrics in svr

--- infodens/classifier/svr_linear.py
+++ infodens/classifier/svr_linear.py
@@ -8,55 +8,65 @@ from infodens.classifier.classifier import Classifier
 from sklearn.svm import LinearSVR
 from sklearn.model_selection import GridSearchCV
 import numpy as np
-from sklearn.metrics import mean_squared_error
-
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from scipy.stats import pearsonr
 import time
 
 
 class SVR_linear(Classifier):
-    '''
-    classdocs
-    '''
+	'''
+	classdocs
+	'''
 
-    classifierName = 'Support Vector Regressor'
-    C = np.logspace(-5.0, 5.0, num=10, endpoint=True, base=2)
+	classifierName = 'Support Vector Regressor'
+	C = np.logspace(-5.0, 5.0, num=10, endpoint=True, base=2)
 
-    def evaluate(self):
+	def evaluate(self):
 
-        """ Overriding default evaluate"""
-        y_pred = self.predict()
-        return mean_squared_error(self.ytest, y_pred)
+		""" Overriding default evaluate"""
+		y_pred = self.predict()
+		mse = mean_squared_error(self.ytest, y_pred)
+		mae = mean_absolute_error(self.ytest, y_pred)
+		r = pearsonr(self.ytest, y_pred) 
+		return mae, mse, r
 
-    def train(self):
+	def train(self):
 
-        tuned_parameters = [{'C': self.C}]
+		tuned_parameters = [{'C': self.C}]
 
-        print ('SVR Optimizing. This will take a while')
-        start_time = time.time()
-        clf = GridSearchCV(LinearSVR(), tuned_parameters,
-                           n_jobs=self.threadCount, cv=5)
+		print ('SVR Optimizing. This will take a while')
+		start_time = time.time()
+		clf = GridSearchCV(LinearSVR(), tuned_parameters,
+						   n_jobs=self.threadCount, cv=5)
 
-        clf.fit(self.Xtrain, self.ytrain)
-        print('Done with Optimizing. it took ', time.time() -
-              start_time, ' seconds')
+		clf.fit(self.Xtrain, self.ytrain)
+		print('Done with Optimizing. it took ', time.time() -
+			  start_time, ' seconds')
 
-        self.model = clf.best_estimator_
+		self.model = clf.best_estimator_
 
-    def runClassifier(self):
-        """ Overriding default running"""
-        mse = []
-        #pre = []; rec = []; fsc = []
+	def runClassifier(self):
+		""" Overriding default running"""
+		all_mse = []
+		all_mae = []
+		all_r = []
+		#pre = []; rec = []; fsc = []
 
-        for i in range(self.n_foldCV):
-            self.shuffle()
-            self.splitTrainTest()
-            self.train()
-            error = self.evaluate()
-            mse.append(error)
+		for i in range(self.n_foldCV):
+			self.shuffle()
+			self.splitTrainTest()
+			self.train()
+			mae, mse, r = self.evaluate()
+			all_mse.append( mse )
+			all_mae.append( mae )
+			all_r.append( r )
 
-        classifReport = 'Average MSE: ' + str(np.mean(mse))
-        #classifReport += '\nAverage Precision: ' + str(np.mean(pre))
-        #classifReport += '\nAverage Recall: ' + str(np.mean(rec))
-        #classifReport += '\nAverage F-score: ' + str(np.mean(fsc))
+		classifReport = "Average MAE: {0}".format( np.mean( all_mae ) )
+		classifReport += "\nAverage MSE: {0}".format( np.mean( all_mse ) )
+		classifReport += "\nAverage Pearson's r: {0}".format( np.mean( all_r ) )
 
-        return classifReport
+		#classifReport += '\nAverage Precision: ' + str(np.mean(pre))
+		#classifReport += '\nAverage Recall: ' + str(np.mean(rec))
+		#classifReport += '\nAverage F-score: ' + str(np.mean(fsc))
+
+		return classifReport
